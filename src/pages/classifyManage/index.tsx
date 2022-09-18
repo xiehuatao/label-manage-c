28fc2cae9c212ad15534b119f9545e0ca6f46a25
hhl <3046373909@qq.com>
2022-09-18 18:34:08 +0800

    黄海龙-初始化文件

---
+++ src/pages/classifyManage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ClassifyManage() {
+
+    return (
+        <div style={{width: "100px", height: "100px", color: "red"}}>
+
+        </div>
+    )
+}
+
+export default ClassifyManage;
